c4737679a107ae25fc12ff3f9af8fdd573a6c9e3
eason <wangyisheng2014@hotmail.com>
2016-12-15 21:03:26 +0800

    增加状态字段

--- ip_proxy_pool/main/spiders/model/spider_running_log.py
+++ ip_proxy_pool/main/spiders/model/spider_running_log.py
@@ -9,14 +9,16 @@ class SpiderCrawlLog(Base):
     id = Column(Integer, Sequence('id', start=1, increment=1), primary_key=True)  # 设定自增长主键
     spiderID = Column(Integer,nullable=False)
     spiderName = Column(String(50),nullable=False)
+    status = Column(String(20), nullable=False)
     startTime = Column(DateTime,nullable=True)
     endTime = Column(DateTime,nullable=True)
     pages = Column(Integer,nullable=False,default=0)
     items = Column(Integer,nullable=False,default=0)
 
-    def __init__(self,spiderID,spiderName,startTime,endTime,pages,items):
+    def __init__(self,spiderID,spiderName,status,startTime,endTime,pages,items):
         self.spiderID = spiderID
         self.spiderName = spiderName
+        self.status = status
         self.startTime = startTime
         self.endTime = endTime
         self.pages = pages
