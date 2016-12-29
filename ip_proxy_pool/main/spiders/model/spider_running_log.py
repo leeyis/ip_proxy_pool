6fae804bc8b77ebb91c727a69199fb94637e5902
eason <wangyisheng2014@hotmail.com>
2016-12-29 14:35:58 +0800

    添加spider运行状态表

--- ip_proxy_pool/main/spiders/model/spider_running_log.py
+++ ip_proxy_pool/main/spiders/model/spider_running_log.py
@@ -9,11 +9,7 @@ class SpiderCrawlLog(Base):
     id = Column(Integer, Sequence('id', start=1, increment=1), primary_key=True)  # 设定自增长主键
     spiderID = Column(Integer,nullable=False)
     spiderName = Column(String(50),nullable=False)
-<<<<<<< HEAD
     status = Column(String(20),nullable=False)
-=======
-    status = Column(String(20), nullable=False)
->>>>>>> c4737679a107ae25fc12ff3f9af8fdd573a6c9e3
     startTime = Column(DateTime,nullable=True)
     endTime = Column(DateTime,nullable=True)
     pages = Column(Integer,nullable=False,default=0)
